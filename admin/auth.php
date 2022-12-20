68317eb20ca5e93a101fffa9579dcccf389ae1dc
MiraiderHZ <hilzhardi.23@gmail.com>
2022-12-20 07:58:17 +0700

    fixed: titles

--- admin/auth.php
+++ admin/auth.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pendaftaran Siswa Baru SMA Gorgom</title>
+    <title>Seleksi Pegawai Baru Kementerian Kelautan dan Perikanan</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="./assets/css/auth.css">
     <link href='https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900' rel='stylesheet'>
