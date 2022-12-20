a1e90d8e0d06ae6f9813fc62a60a1d7d66262f76
MiraiderHZ <hilzhardi.23@gmail.com>
2022-12-20 18:31:11 +0700

    added: user home page

---
+++ home.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seleksi Pegawai Baru Kementerian Kelautan dan Perikanan</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/home.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900' rel='stylesheet'>
+</head>
+
+<body>
+    <div class="merged">
+
+        <!-- Navbar -->
+        <nav>
+            <img src="https://kkp.go.id/an-component/media/upload-gambar-pendukung/kkp/DATA%20KKP/2019/Logo%20KKP/KKP%20Ind.png" alt="Brand Logo" class="brand-logo">
+            <div class="profile">
+                <img src="images/baradus.png" alt="Profile Picture" class="profile-picture">
+                <div class="username-and-id">
+                    <span class="username">Username</span>
+                    <span class="id-number">3525015306780002</span>
+                </div>
+                <a href="daftar.php" class="fw-semibold btn btn-brand me-2" id="logout-btn">Keluar</a>
+            </div>
+        </nav>
+
+        <!-- Card -->
+        <div class="container mb-5 shadow-lg">
+            <div class="row justify-content-center">
+                <div class="card p-5" style="width: 100%;">
+                    <div class="row no-gutters">
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h1 class="card-title" id="main-title">Profil Pendaftar</h1>
+                                <h5 class="card-title" id="card-label">Nama</h5>
+                                <p class="card-text">User name</p>
+                                <h5 class="card-title mt-3" id="card-label">NIK</h5>
+                                <p class="card-text">User email</p>
+                                <h5 class="card-title mt-3" id="card-label">Pilihan Jabatan</h5>
+                                <p class="card-text">User phone number</p>
+                                <h5 class="card-title mt-3" id="card-label">Status Pendaftaran</h5>
+                                <p class="card-text">User phone number</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <img src="images/baradus.png" class="card-img shadow-lg" id="user-photo" alt="User profile picture">
+                        </div>
+                    </div>
+                    <div class="card-footer d-flex justify-content-center">
+                        <a href="#" class="btn mt-4" id="edit-btn">Edit</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Hero -->
+        <!-- <section id="hero" class="min-vh-100 d-flex align-items-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12" id="hero-content">
+
+
+
+
+                    </div>
+                </div>
+            </div>
+        </section> -->
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
