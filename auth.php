13cc294c477effb989ea2b313519a87d06fb6f9f
MiraiderHZ <hilzhardi.23@gmail.com>
2022-12-20 07:29:41 +0700

    feat: auth front page for user

---
+++ auth.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pendaftaran Siswa Baru SMA Gorgom</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/auth.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900' rel='stylesheet'>
+</head>
+
+<body class="fixed">
+    <div class="merged">
+
+        <!-- Hero -->
+        <section id="hero" class="min-vh-100 d-flex align-items-center text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12" id="hero-content">
+                        <img src="https://kkp.go.id/an-component/media/upload-gambar-pendukung/kkp/DATA%20KKP/2019/Logo%20KKP/KKP%20Ind.png" alt="" class="auth-logo mb-4">
+                        <h4 class="mt-0 mb-4" id="subtitle">Dibutuhkan pegawai baru untuk menjalankan tugas di wilayah Provinsi Jawa Timur</h4>
+                        <div>
+                            <a href="form-daftar.php" class="fw-semibold btn btn-brand me-2" id="daftar-btn">Daftar</a>
+                            <a href="list-siswa.php" class="fw-semibold btn btn-light ms-2" id="masuk-btn">Masuk</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
