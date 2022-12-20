97b49b43681a19c334fc7ffe29372e85b14fe6f1
christhopermarcelino <christhopermarcelino@gmail.com>
2022-12-20 23:30:20 +0700

    style: restyling user regsiter page

---
+++ assets/js/daftar.js
@@ -0,0 +1,58 @@
+function navToFirstAccordion() {
+  const hrLeft = document.getElementsByClassName("horizontalLineLeft")[0];
+  const hrRight = document.getElementsByClassName("horizontalLineRight")[0];
+  const navPoints = Array.from(document.getElementsByClassName("navPoint"));
+
+  reset(navPoints, hrLeft, hrRight);
+
+  navPoints[0].classList.remove("bg-gray");
+  navPoints[0].classList.add("bg-brand");
+}
+
+function navToSecondAccordion() {
+  const hrLeft = document.getElementsByClassName("horizontalLineLeft")[0];
+  const hrRight = document.getElementsByClassName("horizontalLineRight")[0];
+  const navPoints = Array.from(document.getElementsByClassName("navPoint"));
+
+  reset(navPoints, hrLeft, hrRight);
+
+  hrLeft.classList.remove("horizontalLineInactive");
+  hrLeft.classList.add("horizontalLineActive");
+
+  navPoints[0].classList.remove("bg-gray");
+  navPoints[1].classList.remove("bg-gray");
+  navPoints[0].classList.add("bg-brand");
+  navPoints[1].classList.add("bg-brand");
+}
+
+function navToThirdAccordion() {
+  const hrLeft = document.getElementsByClassName("horizontalLineLeft")[0];
+  const hrRight = document.getElementsByClassName("horizontalLineRight")[0];
+  const navPoints = Array.from(document.getElementsByClassName("navPoint"));
+
+  reset(navPoints, hrLeft, hrRight);
+
+  hrLeft.classList.remove("horizontalLineInactive");
+  hrRight.classList.remove("horizontalLineInactive");
+  hrLeft.classList.add("horizontalLineActive");
+  hrRight.classList.add("horizontalLineActive");
+
+  navPoints[0].classList.remove("bg-gray");
+  navPoints[1].classList.remove("bg-gray");
+  navPoints[2].classList.remove("bg-gray");
+  navPoints[0].classList.add("bg-brand");
+  navPoints[1].classList.add("bg-brand");
+  navPoints[2].classList.add("bg-brand");
+}
+
+function reset(navPoints, hrLeft, hrRight) {
+  navPoints.forEach((el) => {
+    el.classList.add("bg-gray");
+    el.classList.remove("bg-brand");
+  });
+
+  hrLeft.classList.add("horizontalLineInactive");
+  hrLeft.classList.remove("horizontalLineActive");
+  hrRight.classList.add("horizontalLineInactive");
+  hrRight.classList.remove("horizontalLineActive");
+}
--- daftar.php
+++ daftar.php
@@ -5,83 +5,181 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Seleksi Pegawai Baru Kementerian Kelautan dan Perikanan</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="assets/css/masuk.css">
+
+    <!-- Fonts -->
     <link href='https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900' rel='stylesheet'>
+
+    <!-- Styling -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/daftar.css">
 </head>
 
 <body>
     <div class="merged">
 
         <!-- Login Card -->
-        <section id="hero" class="min-vh-100 d-flex align-items-center">
-            <div class="container">
-                <div class="row mx-5 px-5">
-                    <div class="bg-card mx-5">
-                        <div class="d-flex justify-content-center">
-                            <h1 class="mt-4">Daftar Akun Baru</h1>
+        <section id="hero" class="min-vh-100">
+            <div class="container py-5 d-flex vh-100 align-items-center justify-content-center">
+                <div class="card cardRegister p-5">
+                    <div class="card-body">
+
+                        <div class="px-5 mb-4">
+                            <div class="d-flex gap-5 position-relative justify-content-between">
+                                <div class="horizontalLine horizontalLineInactive horizontalLineLeft"></div>
+                                <div class="horizontalLine horizontalLineInactive horizontalLineRight"></div>
+                                <div class="rounded-circle navPoint position-relative z-10 px-3 py-2 bg-brand">
+                                    1
+                                </div>
+                                <div class="rounded-circle navPoint position-relative z-10 px-3 py-2 bg-gray">
+                                    2
+                                </div>
+                                <div class="rounded-circle navPoint position-relative z-10 px-3 py-2 bg-gray">
+                                    3
+                                </div>
+                            </div>
                         </div>
-                        <div class="d-flex justify-content-center">
-                            <form action="proses_daftar.php" method="POST" enctype="multipart/form-data">
-                                <fieldset class="my-4">
-                                <p>
-                                    <label for="username" class="d-block text-start">Username: </label>
-                                    <input type="text" name="username" class="my-2 py-3 px-2 bg-input" size="48"/>
-                                </p>
-                                <p>
-                                    <label for="password" class="d-block text-start">Password: </label>
-                                    <input type="password" name="password" class="my-2 py-3 px-2 bg-input" size="48"/>
-                                </p>
-                                <p>
-                                    <label for="nik" class="d-block text-start">NIK: </label>
-                                    <input type="text" name="nik" maxlength="16" class="my-2 py-3 px-2 bg-input" size="48"/>
-                                </p>
-                                <p>
-                                    <label for="nama" class="d-block text-start">Nama: </label>
-                                    <input type="text" name="nama" class="my-2 py-3 px-2 bg-input" size="48"/>
-                                </p>
-                                <p>
-                                    <label for="kelamin" class="d-block text-start">Jenis Kelamin: </label>
-                                    <input type="text" name="kelamin" class="my-2 py-3 px-2 bg-input" size="48"/>
-                                </p>
-                                <p>
-                                    <label for="ttl" class="d-block text-start">Tempat dan Tanggal Lahir: </label>
-                                    <div class="d-flex justfiy-content-between"> 
-                                        <input type="text" name="tempat_lahir" maxlength="50" class="my-2 py-3 px-2 bg-input" size="20"/>
-                                        <input type="text" name="tgl_lahir" maxlength="50" class="my-2 py-3 mx-2 px-2 bg-input" size="23"/>
+
+                        <form action="proses_daftar.php" method="POST" enctype="multipart/form-data">
+
+                            <!- Accordion -->
+                                <div class="accordion accordion-flush" id="accordionRegister">
+                                    <div class="bg-transparent">
+                                        <div id="flush-collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionRegister">
+                                            <div class="accordion-body">
+                                                <div class="mb-3">
+                                                    <label for="username" class="form-label">Username</label>
+                                                    <input type="text" class="form-control bg-gray border-0 py-3 text-white" id="username" name="username">
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="email" class="form-label">Email</label>
+                                                    <input type="email" class="form-control bg-gray border-0 py-3 text-white" id="email" name="email">
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="password" class="form-label">Password</label>
+                                                    <input type="password" class="form-control bg-gray border-0 py-3 text-white" id="password" name="password">
+                                                </div>
+                                                <div class="mb-4">
+                                                    <label for="confirmPassword" class="form-label">Confirm Password</label>
+                                                    <input type="password" class="form-control bg-gray border-0 py-3 text-white" id="confirmPassword" name="confirmPassword">
+                                                </div>
+
+                                                <button onclick="navToSecondAccordion()" class="btn collapsed w-100 buttonAccordionPrimary mb-3 fw-bolder" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                                    Next
+                                                </button>
+                                            </div>
+                                        </div>
                                     </div>
-                                </p>
-                                <p>
-                                    <label for="pendidikan" class="d-block text-start">Pendidikan Terakhir: </label>
-                                    <input type="text" name="pendidikan" maxlength="50" class="my-2 py-3 px-2 bg-input" size="48"/>
-                                </p>
-                                <p>
-                                    <label for="alamat" class="d-block text-start">Alamat: </label>
-                                    <textarea name="alamat" class="my-2 py-3 px-2 bg-input" cols='48'></textarea>
-                                </p>
-                                <p>
-                                    <label for="jabatan" class="d-block text-start">Jabatan Pilihan: </label>
-                                    <input type="text" name="jabatan" maxlength="50" class="my-2 py-3 px-2 bg-input" size="48"/>
-                                </p>
-                                <p>
-                                    <div>Foto KTP: </div>
-                                    <div><input type="file" name="foto_ktp" class="my-2 bg-input"></div>
-                                </p>
-                                <p>
-                                    <div>Foto Diri: </div>
-                                    <div><input type="file" name="foto_diri" class="my-2 bg-input"></div>
-                                </p>
-                                <p>
-                                    <div>Ijazah: </div>
-                                    <div><input type="file" name="ijazah" class="my-2 bg-input"></div>
-                                </p>
-                                <p class="d-flex justify-content-center my-4">
-                                    <input type="submit" value="Daftar" name="daftar" class="fw-bold btn btn-brand me-2" id="masuk-btn"/>
-                                </p>
-                                </fieldset>
-                            </form>
-                        </div>
+
+                                    <div class="bg-transparent">
+                                        <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionRegister">
+                                            <div class="accordion-body">
+                                                <div class="mb-3">
+                                                    <label for="nik" class="form-label">NIK</label>
+                                                    <input type="text" class="form-control bg-gray border-0 py-3 text-white" id="nik" name="nik">
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="name" class="form-label">Nama</label>
+                                                    <input type="text" class="form-control bg-gray border-0 py-3 text-white" id="name" name="name">
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="gender" class="form-label">Jenis Kelamin</label>
+                                                    <div class="d-flex align-items-center gap-3">
+                                                        <div class="form-check">
+                                                            <input class="form-check-input" type="radio" name="gender" id="genderMale">
+                                                            <label class="form-check-label text-white" for="genderMale">
+                                                                Laki-Laki
+                                                            </label>
+                                                        </div>
+                                                        <div class="form-check">
+                                                            <input class="form-check-input" type="radio" name="gender" id="genderFemale">
+                                                            <label class="form-check-label text-white" for="genderFemale">
+                                                                Perempuan
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="mb-3 row">
+                                                    <label class="form-label">Tempat dan Tanggal Lahir</label>
+                                                    <div class="col-12 col-lg-6">
+                                                        <input type="text" class="form-control bg-gray border-0 py-3 text-white" id="birthCity" placeholder="Kota Kelahiran">
+                                                    </div>
+                                                    <div class="col-12 col-lg-6">
+                                                        <input type="DATE" class="form-control bg-gray border-0 py-3 text-white" id="birthDate">
+                                                    </div>
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="education" class="form-label">Pendidikan Terakhir</label>
+                                                    <select class="form-select bg-gray border-0 py-3 text-white" id="education" name="education">
+                                                        <option selected value="">-</option>
+                                                        <option value="SMA/SMK">SMA/SMK</option>
+                                                        <option value="S1">S1</option>
+                                                        <option value="S2">S2</option>
+                                                        <option value="S3">S3</option>
+                                                    </select>
+                                                </div>
+                                                <div class="mb-4">
+                                                    <label for="address" class="form-label">Alamat</label>
+                                                    <input type="text" class="form-control bg-gray border-0 py-3 text-white" id="address" name="address">
+                                                </div>
+
+                                                <button onclick="navToThirdAccordion()" class="btn collapsed w-100 buttonAccordionPrimary mb-3 fw-bolder" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                                                    Next
+                                                </button>
+
+                                                <button onclick="navToFirstAccordion()" class="btn collapsed w-100 buttonAccordionSecondary fw-bolder" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                                    Back
+                                                </button>
+
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="bg-transparent">
+                                        <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionRegister">
+                                            <div class="accordion-body">
+                                                <div class="mb-3">
+                                                    <label for="position" class="form-label">Jabatan Pilihan</label>
+                                                    <select class="form-select bg-gray border-0 py-3 text-white" id="position" name="position">
+                                                        <option selected value="">-</option>
+                                                        <option value="Teknisi Lapangan">Teknisi Lapangan</option>
+                                                        <option value="Operator">Operator</option>
+                                                        <option value="Administrator">Administrator</option>
+                                                        <option value="Supervisor">Supervisor</option>
+                                                    </select>
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="ktp" class="form-label">KTP</label>
+                                                    <label for="ktp" class="form-label btn w-100 py-3 bg-gray d-flex justify-content-between"><span></span><span class="ms-0 bg-brand h-100 px-3 rounded">Upload</span></label>
+                                                    <input style="display:none;" class="form-control bg-gray border-0 form-control-lg" type="file" id="ktp" name="ktp">
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="profilePhoto" class="form-label">Foto Diri</label>
+                                                    <label for="profilePhoto" class="form-label btn w-100 py-3 bg-gray d-flex justify-content-end"><span></span><span class="ms-0 bg-brand h-100 px-3 rounded">Upload</span></label>
+                                                    <input style="display:none;" class="form-control bg-gray border-0 form-control-lg" type="file" id="profilePhoto" name="profilePhoto">
+                                                </div>
+                                                <div class="mb-4">
+                                                    <label for="certificate" class="form-label">Ijazah</label>
+                                                    <label for="certificate" class="form-label btn w-100 py-3 bg-gray d-flex justify-content-end"><span></span><span class="ms-0 bg-brand h-100 px-3 rounded">Upload</span></label>
+                                                    <input style="display:none;" class="form-control bg-gray border-0 form-control-lg" type="file" id="certificate" name="certificate">
+                                                </div>
+
+                                                <button class="btn collapsed w-100 buttonAccordionPrimary mb-3 fw-bolder" type="submit">
+                                                    Submit
+                                                </button>
+
+                                                <button onclick="navToSecondAccordion()" class="btn collapsed w-100 buttonAccordionSecondary fw-bolder" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                                    Back
+                                                </button>
+
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                        </form>
                     </div>
                 </div>
             </div>
@@ -90,6 +188,7 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="assets/js/daftar.js"></script>
 </body>
 
 </html>
