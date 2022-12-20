7ee95162d15a727b621754c390cabc6090efc663
MiraiderHZ <hilzhardi.23@gmail.com>
2022-12-20 21:34:29 +0700

    feat: data fetching in user home page

--- home.php
+++ home.php
@@ -1,66 +1,96 @@
-<!DOCTYPE html>
-<html>
+<?php
+include 'config.php';
+session_start();
+if (isset($_SESSION["username"])) {
+?>
+    <!DOCTYPE html>
+    <html>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Seleksi Pegawai Baru Kementerian Kelautan dan Perikanan</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="assets/css/home.css">
-    <link href='https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900' rel='stylesheet'>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<body>
-    <div class="merged">
+        <title>Seleksi Pegawai Baru Kementerian Kelautan dan Perikanan</title>
 
-        <!-- Navbar -->
-        <nav>
-            <img src="https://kkp.go.id/an-component/media/upload-gambar-pendukung/kkp/DATA%20KKP/2019/Logo%20KKP/KKP%20Ind.png" alt="Brand Logo" class="brand-logo">
-            <div class="profile">
-                <img src="images/baradus.png" alt="Profile Picture" class="profile-picture">
-                <div class="username-and-id">
-                    <span class="username">BaradouzJeh</span>
-                    <span class="id-number">3525015306780002</span>
+        <!-- Fonts -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+        <!-- Styling -->
+        <link rel="stylesheet" type="text/css" href="assets/css/home.css">
+        <link href='https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900' rel='stylesheet'>
+
+    </head>
+
+    <body>
+        <div class="merged">
+            <?php
+            $query = "SELECT * FROM pendaftar WHERE username = :username";
+            $statement = $pdo->prepare($query);
+            $username = $_SESSION["username"];
+            $statement->execute(['username' => $username]);
+            $user = $statement->fetch();
+            ?>
+
+            <!-- Navbar -->
+            <nav>
+                <img src="https://kkp.go.id/an-component/media/upload-gambar-pendukung/kkp/DATA%20KKP/2019/Logo%20KKP/KKP%20Ind.png" alt="Brand Logo" class="brand-logo">
+                <div class="profile">
+                    <img src="images/<?= $user['foto_diri'] ?>" alt="Profile Picture" class="profile-picture">
+                    <div class="username-and-id">
+                        <span class="username"><?= $_SESSION["username"] ?></span>
+                        <span class="id-number"><?= $user["nik"] ?></span>
+                    </div>
+                    <a href="logout.php" class="fw-semibold btn btn-brand me-2" id="logout-btn">Keluar</a>
                 </div>
-                <a href="daftar.php" class="fw-semibold btn btn-brand me-2" id="logout-btn">Keluar</a>
-            </div>
-        </nav>
+            </nav>
 
-        <!-- Card -->
-        <div class="container mb-5 shadow-lg">
-            <div class="row justify-content-center">
-                <div class="card p-5" style="width: 100%;">
-                    <div class="row no-gutters">
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h1 class="card-title" id="main-title">Profil Pendaftar</h1>
-                                <h5 class="card-title" id="card-label">Nama</h5>
-                                <p class="card-text">Banyu Tirta Baradus</p>
-                                <h5 class="card-title mt-3" id="card-label">NIK</h5>
-                                <p class="card-text">3525015306780002</p>
-                                <h5 class="card-title mt-3" id="card-label">Pilihan Jabatan</h5>
-                                <p class="card-text">Supervisor</p>
-                                <h5 class="card-title mt-3" id="card-label">Status Pendaftaran</h5>
-                                <p class="card-text">Belum Diverifikasi</p>
+            <!-- Card -->
+            <div class="container mb-5 shadow-lg">
+                <div class="row justify-content-center">
+                    <div class="card p-5" style="width: 100%;">
+                        <div class="row no-gutters">
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h1 class="card-title" id="main-title">Profil Pendaftar</h1>
+                                    <h5 class="card-title" id="card-label">Nama</h5>
+                                    <p class="card-text"><?= $user["nama"] ?></p>
+                                    <h5 class="card-title mt-3" id="card-label">NIK</h5>
+                                    <p class="card-text"><?= $user["nik"] ?></p>
+                                    <h5 class="card-title mt-3" id="card-label">Pilihan Jabatan</h5>
+                                    <p class="card-text"><?= $user["jabatan"] ?></p>
+                                    <h5 class="card-title mt-3" id="card-label">Status Pendaftaran</h5>
+                                    <?php if ($user["status"] === null) {
+                                        echo '<p class="card-text">Belum Diverifikasi</p>';
+                                    } else if ($user["status"] == 'Lolos Berkas') {
+                                        echo '<p class="card-text" id="card-text-green">Lolos Berkas</p>';
+                                    } else if ($user["status"] == 'Tidak Lolos Berkas') {
+                                        echo '<p class="card-text" id="card-text-red">Tidak Lolos Berkas</p>';
+                                    } ?>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <img src="images/<?= $user['foto_diri'] ?>" class="card-img shadow-lg" id="user-photo" alt="User profile picture">
                             </div>
                         </div>
-                        <div class="col-md-4">
-                            <img src="images/baradus.png" class="card-img shadow-lg" id="user-photo" alt="User profile picture">
+                        <div class="card-footer d-flex justify-content-center">
+                            <?php if ($user["status"] === null || $user["status"] == 'Tidak Lolos Berkas') {
+                                echo '<a href="#" class="btn mt-4" id="edit-btn">Edit</a>';
+                            } else if ($user["status"] == 'Lolos Berkas') { 
+                                    echo '<a href="#" class="btn mt-4" id="kartu-btn">Lihat Kartu Ujian</a>';
+                            } ?>
                         </div>
-                    </div>
-                    <div class="card-footer d-flex justify-content-center">
-                        <a href="#" class="btn mt-4" id="edit-btn">Edit</a>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-</body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    </body>
 
-</html>
-
-
-
+    </html>
+<?php
+} else {
+    header("location: masuk.php");
+}
+?>
--- masuk.php
+++ masuk.php
@@ -1,3 +1,40 @@
+<?php
+
+include 'config.php';
+
+error_reporting(0);
+
+session_start();
+
+$message = "";
+try {
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if (isset($_POST["login"])) {
+        if (empty($_POST["username"]) || empty($_POST["password"])) {
+            $message = '<label class="d-flex justify-content-center">All fields are required</label>';
+        } else {
+            $query = "SELECT * FROM pendaftar WHERE username = :username AND password = :password";
+            $statement = $pdo->prepare($query);
+            $statement->execute(
+                array(
+                    'username'     =>     $_POST["username"],
+                    'password'     =>     $_POST["password"]
+                )
+            );
+            $count = $statement->rowCount();
+            if ($count > 0) {
+                $_SESSION["username"] = $_POST["username"];
+                header("location: home.php");
+            } else {
+                $message = '<label class="d-flex justify-content-center">Wrong Data</label>';
+            }
+        }
+    }
+} catch (PDOException $error) {
+    $message = $error->getMessage();
+}
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -18,24 +55,29 @@
         <section id="hero" class="min-vh-100 d-flex align-items-center">
             <div class="container">
                 <div class="row mx-5 px-5">
+                    <?php
+                    if (isset($message)) {
+                        echo '<label class="text-danger d-flex justify-content-center align-items-center">' . $message . '</label>';
+                    }
+                    ?>
                     <div class="bg-card mx-5">
                         <div class="d-flex justify-content-center">
                             <h1 class="mt-4">Login</h1>
                         </div>
                         <div class="d-flex justify-content-center">
-                            <form action="proses_simpan.php" method="POST" enctype="multipart/form-data">
+                            <form method="POST" enctype="multipart/form-data">
                                 <fieldset class="my-4">
-                                <p>
-                                    <label for="username" class="d-block text-start">Username: </label>
-                                    <input type="text" name="username" maxlength="50" class="my-2 py-3 bg-input" size="48"/>
-                                </p>
-                                <p>
-                                    <label for="password" class="d-block text-start">Password: </label>
-                                    <input type="password" name="password" maxlength="50" class="my-2 py-3 bg-input" size="48"/>
-                                </p>
-                                <p class="d-flex justify-content-center my-4">
-                                    <input type="submit" value="Masuk" name="masuk" class="fw-bold btn btn-brand me-2" id="masuk-btn"/>
-                                </p>
+                                    <p>
+                                        <label for="username" class="d-block text-start">Username: </label>
+                                        <input type="text" name="username" maxlength="50" class="my-2 py-3 bg-input" size="48" />
+                                    </p>
+                                    <p>
+                                        <label for="password" class="d-block text-start">Password: </label>
+                                        <input type="password" name="password" maxlength="50" class="my-2 py-3 bg-input" size="48" />
+                                    </p>
+                                    <p class="d-flex justify-content-center my-4">
+                                        <input type="submit" value="Masuk" name="login" class="fw-bold btn btn-brand me-2" id="masuk-btn" />
+                                    </p>
                                 </fieldset>
                             </form>
                         </div>
